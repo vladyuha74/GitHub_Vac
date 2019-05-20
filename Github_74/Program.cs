3f20964a41f9aab7356d4a67c3531869ee1756d4
vladyuha74 <xXx.vladyuha74.xXx@gmail.ru>
2019-05-20 12:43:56 +0500

    Массив

--- Github_74/Program.cs
+++ Github_74/Program.cs
@@ -10,7 +10,31 @@ namespace Github_74
     {
         static void Main(string[] args)
         {
-            Console.Write("HELLO WORLD");
+            int[] array = { 4, 0, 3, 19, 492, -10 };
+            int min_chislo1 =1000000; // Дам саоме больше значение которого нет в массиве
+            int min_chislo2 = 1000000; // Дам саоме больше значение которого нет в массиве
+            int summa = 0; //Переменная для суммы двух чисел
+            for (int i=0; i < array.Length; i++) // Ищем первое число.
+            {
+                if (array[i] < min_chislo1)
+                {
+                    min_chislo1 = array[i];
+                }  
+            }
+            for(int j=0; j < array.Length; j++) //Ищем второе число.
+            {
+                if ((array[j] < min_chislo2) & (array[j] != min_chislo1)) // Минимальное 1 число исключаем и задаем условие чтобы сравнивалось минимальным вторым числом.
+                {
+                    if (min_chislo2 > min_chislo1)
+                    {
+                        min_chislo2 = array[j];
+                    }
+                }
+            }
+            summa = min_chislo1 + min_chislo2; // Сумируем минимальные числа
+            //summa = summa + min_chislo;
+            Console.Write("Итого: " + min_chislo1 + "+" + min_chislo2 + " = " + summa); // В итоге должно быть -10+0 = -10
+            Console.ReadKey();
         }
     }
 }
